d321487745eed459f536516bbf23f1ce02d1c6d5
wyk516303250 <516303250@qq.com>
2019-05-29 20:51:28 +0800

    代码上传

---
+++ zuoye4/src/main/java/com/example/zuoye4/fragment/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.zuoye4.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zuoye4.R;
+
+/**
+ * Created by 康康 on 2019/5/28.
+ */
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.fragment_home, null, false);
+        return inflate;
+    }
+}
